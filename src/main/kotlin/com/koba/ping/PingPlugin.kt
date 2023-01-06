d9c9c1753c1a4fc325d0e93ebaa2bfa5b973e4ce
komq-babo <71371504+komq-babo@users.noreply.github.com>
2023-01-06 16:14:11 +0900

    Update PingPlugin.kt

--- src/main/kotlin/com/koba/ping/PingPlugin.kt
+++ src/main/kotlin/com/koba/ping/PingPlugin.kt
@@ -24,30 +24,68 @@ class PingPlugin: JavaPlugin(), Listener {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (label.equals("ping", true)) {
-            ping = if (ping) {
-                Bukkit.broadcastMessage("§e독일핑이 비활성화 되었습니다.")
-                false
-            } else {
-                Bukkit.broadcastMessage("§e독일핑이 활성화 되었습니다.")
-                server.scheduler.runTaskTimer(this, PingTask(), 0L , 1L)
-                pingspeed = config.getInt("ping").toDouble()
-                true
-            }
-        }
-        if (label.equals("setping", true)) {
+
             if (args.isNotEmpty()) {
-                if (args[0].matches("[+-]?\\d*(\\.\\d+)?".toRegex())) {
-                    pingspeed = java.lang.Double.valueOf(args[0])
-                    config["ping"] = pingspeed
-                    saveConfig()
-                    Bukkit.broadcastMessage(String.format("§a지연 시간을 " + pingspeed + "ms로 설정했습니다."))
-                } else {
-                    sender.sendMessage("§e/setping <ping>")
+                    if (args[0] == "toggle") {
+                        if (sender.hasPermission("ping.command")) {
+                            ping = if (ping) {
+                                Bukkit.broadcastMessage("§e독일핑이 비활성화 되었습니다.")
+                                false
+                            } else {
+                                Bukkit.broadcastMessage("§e독일핑이 활성화 되었습니다.")
+                                server.scheduler.runTaskTimer(this, PingTask(), 0L, 1L)
+                                pingspeed = config.getInt("ping").toDouble()
+                                true
+                            }
+                        } else {
+                            sender.sendMessage("§c명령어를 쓸 수 있는 권한이 없습니다.")
+                        }
+                    }
+
+                    if (args[0] == "set") {
+                        if (sender.hasPermission("ping.command")) {
+                            if (args.size == 2) {
+                                if (args[1].matches("[+-]?\\d*(\\.\\d+)?".toRegex())) {
+                                    pingspeed = java.lang.Double.valueOf(args[1])
+                                    config["ping"] = pingspeed
+                                    saveConfig()
+                                    Bukkit.broadcastMessage(String.format("§a지연 시간을 " + pingspeed + "ms로 설정했습니다."))
+                                } else {
+                                    sender.sendMessage("/ping toggle")
+                                    sender.sendMessage("/ping set [Int]")
+                                    sender.sendMessage("/ping check")
+                                }
+                            } else {
+                                sender.sendMessage("/ping toggle")
+                                sender.sendMessage("/ping set [Int]")
+                                sender.sendMessage("/ping check")
+                            }
+
+                        } else {
+                            sender.sendMessage("§c명령어를 쓸 수 있는 권한이 없습니다.")
+                        }
+                    }
+
+                if (args[0] == "check") {
+                    pingspeed = config.getInt("ping").toDouble()
+                    sender.sendMessage("§l----------독일핑----------")
+                    if (ping) sender.sendMessage("§l독일핑 : §a켜짐")
+                    if (!ping) sender.sendMessage("§l독일핑 : §c꺼짐")
+                    if (pingspeed.toInt() <= 100) sender.sendMessage("§l지연시간 : §a$pingspeed§ams")
+                    if (pingspeed.toInt() == 200) sender.sendMessage("§l지연시간 : §e$pingspeed§ems")
+                    if (pingspeed.toInt() >= 300) sender.sendMessage("§l지연시간 : §c$pingspeed§cms")
                 }
-            } else {
-                sender.sendMessage("§e/setping <ping>")
+                return false
+
+                sender.sendMessage("/ping toggle")
+                sender.sendMessage("/ping set [Int]")
+                sender.sendMessage("/ping check")
             }
-            return false
+
+            sender.sendMessage("/ping toggle")
+            sender.sendMessage("/ping set [Int]")
+            sender.sendMessage("/ping check")
+
         }
         return false
     }
@@ -65,5 +103,3 @@ class PingTask : Runnable {
         }
     }
 }
-
-
