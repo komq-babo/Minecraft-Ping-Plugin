dcb6770b062363155623fad18a890a2e695c6c5b
komq-babo <71371504+komq-babo@users.noreply.github.com>
2023-02-06 15:15:48 +0900

    Kommand 파일 분리화

--- src/main/kotlin/io/github/koba/ping/PingPlugin.kt
+++ src/main/kotlin/io/github/koba/ping/PingPlugin.kt
@@ -9,10 +9,17 @@ import java.io.File
 var ping = false
 var pingspeed = 0.0
 
-class PingPlugin: JavaPlugin() {
+class PingPlugin : JavaPlugin() {
+
+    companion object {
+        lateinit var instance : PingPlugin
+    }
+
+    init {
+        instance = this
+    }
 
     override fun onEnable() {
-
         saveConfig()
         val cfile = File(dataFolder, "config.yml")
         if (cfile.length() == 0L) {
@@ -24,59 +31,11 @@ class PingPlugin: JavaPlugin() {
 
         server.scheduler.runTaskTimer(this, PingTask(), 0L, 1L)
 
-        kommand {
-            register("ping") {
+        setupCommands()
+    }
 
-                then("toggle") {
-                    requires { isOp }
-
-                    executes {
-                        ping = if (ping) {
-                            Bukkit.broadcastMessage("§e독일핑이 비활성화 되었습니다.")
-                            false
-                        } else {
-                            Bukkit.broadcastMessage("§e독일핑이 활성화 되었습니다.")
-                            true
-                        }
-                    }
-                }
-
-                then("set") {
-                    requires { isOp }
-
-                    then("pingdouble" to double()) {
-                        executes {
-                            val pingdouble: Double by it
-
-                            if (pingdouble < 0) {
-                                sender.sendMessage("§c0보다 작은 수는 입력할 수 없습니다.")
-                            } else {
-
-                                pingspeed = pingdouble
-                                config["ping"] = pingspeed
-                                saveConfig()
-                                Bukkit.broadcastMessage(String.format("§a지연 시간을 " + pingspeed + "ms로 설정했습니다."))
-                            }
-                        }
-                    }
-                }
-
-                then("check") {
-                    executes {
-                        pingspeed = config.getDouble("ping")
-
-                        sender.sendMessage("§l----------독일핑----------")
-
-                        if (ping) sender.sendMessage("§l독일핑 : §a켜짐")
-                        if (!ping) sender.sendMessage("§l독일핑 : §c꺼짐")
-
-                        if (pingspeed < 200) sender.sendMessage("§l지연시간 : §a$pingspeed§ams")
-                        if (pingspeed >= 200 && pingspeed < 300) sender.sendMessage("§l지연시간 : §e$pingspeed§ems")
-                        if (pingspeed >= 300) sender.sendMessage("§l지연시간 : §c$pingspeed§cms")
-                    }
-                }
-            }
-        }
+    private fun setupCommands() = kommand {
+        KommandPing.register(this@PingPlugin, this)
     }
 }
 
